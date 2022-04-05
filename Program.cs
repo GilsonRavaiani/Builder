5795340bed2b07ccaa873116ba4fc50acea342cc
GilsonRavaiani <gravaiani@yahoo.com.br>
2022-04-05 13:38:46 -0300

    StringBuilder

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using System.Text;
+
+namespace StringBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = new StringBuilder();//Novo Construtor(Build)(Temos sempre a cópia e nunca a referencia)
+            texto.Append("Este texto é um teste");
+            texto.Append("Este texto é ");
+            texto.Append("é um teste");
+            texto.Append("Este é teste");
+
+            texto.ToString();
+            Console.WriteLine(texto);
+
+
+
+
+
+            //var texto = "Este texto é um teste";
+            //texto += "aqui";  (Concatenacao) Unir duas Strings 
+
+
+            //Console.WriteLine("Hello World!");
+
+        }
+    }
+}
